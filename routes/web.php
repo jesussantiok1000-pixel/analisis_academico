89fb2a826cfbecf7f9a3ed70620968701501941e
jesus <jesussantiok1000@gmail.com>
2026-04-15 21:30:58 +0200

    Prueba ruta simple

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
+
+
+
+ 
 <?php
 
-use App\Http\Controllers\DashboardController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', DashboardController::class);
+Route::get('/', function () {
+    return 'Laravel funcionando en Render';
+});
