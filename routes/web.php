e02eee6e42b8fcbb96cbba73b91cef8bff5a12ee
jesus <jesussantiok1000@gmail.com>
2026-04-15 22:10:42 +0200

    Restaurar ruta dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 
 
-
- 
 <?php
 
-
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Laravel funcionando en Render';
-});
+Route::get('/', DashboardController::class);
